e9a11600093ae411f06fa9d47e2d472232f04618
yoel3imari <youssef.elaimari@gmail.com>
2025-04-11 21:09:53 +0100

    service gRPC

--- frontend/src/router/index.ts
+++ frontend/src/router/index.ts
@@ -1,9 +1,9 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
-import LoginView from '@/views/LoginView.vue'
-import RegisterView from '@/views/RegisterView.vue'
-import ChatView from '@/views/ChatView.vue'
-import SettingsView from '@/views/SettingsView.vue'
+import HomeView from '../views/user/HomeView.vue'
+import LoginView from '@/views/auth/LoginView.vue'
+import RegisterView from '@/views/auth/RegisterView.vue'
+import ChatView from '@/views/user/ChatView.vue'
+import SettingsView from '@/views/user/SettingsView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
