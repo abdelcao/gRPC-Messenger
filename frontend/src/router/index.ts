9e394e360f64c6c21cea53f8bce33952e97f8139
yoel3imari <youssef.elaimari@gmail.com>
2025-04-13 17:51:42 +0100

    routing

--- frontend/src/router/index.ts
+++ frontend/src/router/index.ts
@@ -3,12 +3,11 @@ import HomeView from '../views/user/HomeView.vue'
 import LoginView from '@/views/auth/LoginView.vue'
 import RegisterView from '@/views/auth/RegisterView.vue'
 import ChatView from '@/views/user/ChatView.vue'
-import SettingsView from '@/views/user/SettingsView.vue'
-import DashboardView from '@/views/admin/UsersView.vue'
 import AdminLayout from '@/layouts/AdminLayout.vue'
 import OverviewView from '@/views/admin/OverviewView.vue'
 import UsersView from '@/views/admin/UsersView.vue'
 import ReportsView from '@/views/admin/ReportsView.vue'
+import SettingsView from '@/views/admin/SettingsView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
