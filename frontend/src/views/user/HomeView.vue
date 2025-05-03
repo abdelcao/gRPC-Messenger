4c5618c6a9d17e16a27b0d16c3ad15c3425aee3d
yoel3imari <youssef.elaimari@gmail.com>
2025-05-03 16:05:48 +0100

    saved

--- frontend/src/views/user/HomeView.vue
+++ frontend/src/views/user/HomeView.vue
@@ -4,8 +4,8 @@ import MainLayout from "@/layouts/MainLayout.vue"
 </script>
 
 <template>
-  <!-- <MainLayout> -->
-    <!-- <div>main</div> -->
-    <Check />
-  <!-- </MainLayout> -->
+  <MainLayout>
+    <div>main</div>
+    <!-- <Check /> -->
+  </MainLayout>
 </template>
