c243238d2df851604eaad2c8ad0adbf4ebe1aa94
Darkfazer <126478274+Horese07@users.noreply.github.com>
2025-05-06 00:21:24 +0100

    yasser's changes

--- frontend/src/components/ChatWindow.vue
+++ frontend/src/components/ChatWindow.vue
@@ -1,34 +1,24 @@
 <!-- src/components/ChatWindow.vue -->
 <template>
-  <div class="flex-1 flex flex-col bg-white dark:bg-gray-800">
-    <!-- Header -->
-    <div class="flex items-center justify-between px-4 py-3 border-b border-gray-500">
-      <div class="flex items-center gap-2">
-        <img src="/images/default_avatar.jpg" class="w-10 h-10 rounded-full" />
-        <div class="flex-1">
-          <div class="font-semibold text-sm">Selected Chat</div>
-          <div class="text-xs text-gray-400">Online</div>
-        </div>
-      </div>
-    </div>
 
-    <!-- Messages -->
-    <div class="flex-1 p-4 overflow-y-auto space-y-2">
-      <MessageBubble time="12:38">Hi, I’ve just finished my report.</MessageBubble>
-      <MessageBubble :isOwn="true" time="12:42">Great job! How long did it take you?</MessageBubble>
-      <MessageBubble time="12:49">About two hours. I had to check some data…</MessageBubble>
-      <MessageBubble :isOwn="true" time="13:05">I have a question about the report…</MessageBubble>
-    </div>
+  <!-- Header -->
+  <ChatHeader />
 
-    <!-- Input -->
-    <ChatSender />
-
+  <!-- Messages -->
+  <div class="flex-1 p-4 overflow-y-auto space-y-2">
+    <MessageBubble time="12:38">Hi, I’ve just finished my report.</MessageBubble>
+    <MessageBubble :seen="true" :isOwn="true" time="12:42">Great job! How long did it take you?</MessageBubble>
+    <MessageBubble time="12:49">About two hours. I had to check some data…</MessageBubble>
+    <MessageBubble :isOwn="true" time="13:05">I have a question about the report…</MessageBubble>
   </div>
+
+  <!-- Input -->
+  <ChatSender />
 </template>
 
 <script setup lang="ts">
-import { ref } from 'vue'
 import MessageBubble from './MessageBubble.vue'
-import ChatSender from './ChatSender.vue'
+import ChatSender from './ChatSender.vue';
+import ChatHeader from './ChatHeader.vue';
 
 </script>
---
+++ frontend/src/components/TheSidebar.vue
@@ -0,0 +1,84 @@
+<template>
+    <div
+      class="w-16 bg-white dark:bg-gray-800 border-r border-gray-500 flex flex-col items-center py-4"
+    >
+      <div class="space-y-4 flex flex-col">
+        <div
+          v-for="(elm, n) in elms.top"
+          :key="n"
+          class="cursor-pointer p-4  rounded-full flex items-center justify-center"
+          :class="
+            active === elm.name
+              ? 'bg-lime-500/25 text-lime-200 '
+              : 'bg-gray-200 dark:bg-gray-700 hover:bg-lime-500/25 hover:text-lime-200'
+          "
+          @click="elm.action"
+        >
+          <i :class="elm.icon" style="font-size: 1.125rem;" />
+        </div>
+      </div>
+      <div class="mt-auto">
+        <div
+          v-for="(elm, n) in elms.bottom"
+          :key="n"
+          class="cursor-pointer p-4 bg-gray-200 dark:bg-gray-700 rounded-full flex items-center justify-center"
+          :class="
+            active === elm.name
+              ? 'bg-lime-500/25 text-lime-200'
+              : 'bg-gray-200 dark:bg-gray-700 hover:bg-lime-500/25 hover:text-lime-200'
+          "
+          @click="elm.action"
+        >
+          <i :class="elm.icon"  style="font-size: 1.125rem;" />
+        </div>
+      </div>
+    </div>
+    <SettingsModal />
+  </template>
+  
+  <script setup lang="ts">
+  import { useAppStore } from '@/stores/app'
+  import SettingsModal from './SettingsModal.vue'
+  import { ref } from 'vue'
+  
+  const appStore = useAppStore()
+  const active = ref('chat');
+  
+  const elms = {
+    top: [
+      {
+        icon: 'pi pi-bell',
+        action: () => {
+          active.value = 'notification'
+          appStore.activePanel = 'notification'
+        },
+        name: 'notification'
+      },
+      {
+        icon: 'pi pi-comments',
+        action: () => {
+          active.value = 'chat'
+          appStore.activePanel = 'chat'
+        },
+        name: 'chat'
+      },
+      {
+        icon: 'pi pi-users',
+        action: () => {
+          active.value = 'group'
+          appStore.activePanel = 'group'
+        },
+        name: 'group'
+      },
+    ],
+    bottom: [
+      {
+        icon: 'pi pi-cog',
+        action: () => {
+          appStore.settingsModalVisible = true
+        },
+        name: 'settings'
+      },
+    ],
+  }
+  </script>  
