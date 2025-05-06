90a198fb2e794b51f894232cdde92ea7b62712e7
Darkfazer <yasser.daoud@edu.uiz.ac.ma>
2025-05-06 15:30:54 +0100

    yasser's second changes

--- frontend/src/components/ChatHeader.vue
+++ frontend/src/components/ChatHeader.vue
@@ -1,27 +1,7 @@
-<script setup>
-import { ref } from 'vue'
-import Popover from 'primevue/popover'
+<script setup lang="ts">
+import { useAppStore } from '@/stores/app'
 
-const op = ref()
-const members = ref([
-  { name: 'Amy Elsner', image: 'amyelsner.png', email: 'amy@email.com', role: 'Owner' },
-  {
-    name: 'Bernardo Dominic',
-    image: 'bernardodominic.png',
-    email: 'bernardo@email.com',
-    role: 'Editor',
-  },
-  {
-    name: 'Ioni Bowcher',
-    image: 'ionibowcher.png',
-    email: 'ioni@email.com',
-    role: 'Viewer',
-  },
-])
-
-const toggle = (event) => {
-  op.value.toggle(event)
-}
+const appStore = useAppStore()
 </script>
 
 <template>
@@ -30,23 +10,16 @@ const toggle = (event) => {
       <img src="/images/default_avatar.jpg" class="w-10 h-10 rounded-full" />
       <div class="flex-1">
         <div class="font-semibold text-sm">Selected Chat</div>
-        <div class="text-xs text-gray-400">Online</div>
+        <div class="text-xs text-lime-400">Online</div>
       </div>
     </div>
 
     <div>
       <i
-        class="pi pi-ellipsis-v p-2 flex items-center justify-center hover:bg-gray-500 rounded-full"
-        @click="toggle"
+        @click="() => (appStore.panelInforOpened = !appStore.panelInforOpened)"
+        style="font-size: 1.25rem"
+        class="pi pi-info-circle cursor-pointer p-2 flex items-center justify-center hover:bg-gray-500 rounded-full"
       ></i>
-
-      <Popover ref="op">
-        <div class="flex flex-col gap-4 rounded-tr-none">
-          <div>
-            <span class="font-medium block mb-2">Report</span>
-          </div>
-        </div>
-      </Popover>
     </div>
   </div>
 </template>
--- frontend/src/components/MessageBubble.vue
+++ frontend/src/components/MessageBubble.vue
@@ -3,12 +3,18 @@
   <div :class="['mb-2 flex', isOwn ? 'justify-end' : 'justify-start']">
     <div
       :class="[
-        'rounded-lg px-4 py-2 text-sm max-w-[70%]',
-        isOwn ? 'bg-blue-500 text-white' : 'bg-gray-100 text-gray-800'
+        'rounded-lg px-2.5 py-2 text-sm max-w-[70%] font-semibold',
+        isOwn ? 'bg-lime-500/25 text-white' : 'bg-gray-100 text-gray-900',
       ]"
     >
       <slot />
-      <div class="text-[10px] text-right mt-1" v-if="time">{{ time }}</div>
+      <div
+        class="flex items-center justify-end gap-3"
+        :class="isOwn ? 'text-lime-500' : 'text-gray-500'"
+      >
+      <span v-if="time" class="text-xs font-semibold text-right">{{ time }}</span>
+      <span v-if="seen" class="text-xs"> seen </span>
+      </div>
     </div>
   </div>
 </template>
@@ -17,5 +23,6 @@
 defineProps<{
   isOwn?: boolean
   time?: string
+  seen?: boolean
 }>()
 </script>
