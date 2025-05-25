4f671cb4362053016d81261a516da9b0cef08e20
yoel3imari <youssef.elaimari@gmail.com>
2025-05-25 16:59:11 +0100

    notifications

--- frontend/src/layouts/MainLayout.vue
+++ frontend/src/layouts/MainLayout.vue
@@ -41,29 +41,29 @@ onMounted(async () => {
 
       /*****    get notifications   ****** */
 
-      // const res = await notifService.getAllNotifications({ userId: authStore.user.id.toString() })
-      // res.notifications.forEach((n) => {
-      //   notifStore.pushNotif(n)
-      //   if (n.unread) {
-      //     notifStore.unreadCount++
-      //   }
-      // })
+      const res = await notifService.getAllNotifications({ userId: authStore.user.id.toString() })
+      res.notifications.forEach((n) => {
+        notifStore.pushNotif(n)
+        if (n.unread) {
+          notifStore.unreadCount++
+        }
+      })
 
-      // const stream = notifService.streamNotifications({
-      //   userId: authStore.user.id.toString(),
-      // }) as AsyncIterable<Notification>
+      const stream = notifService.streamNotifications({
+        userId: authStore.user.id.toString(),
+      }) as AsyncIterable<Notification>
 
-      // for await (const notification of stream) {
-      //   notifStore.pushNotif(notification)
-      //   toast.add({
-      //     severity: 'info',
-      //     detail: notification.content,
-      //     summary: notification.title,
-      //   })
-      //   if (notification.unread) {
-      //     notifStore.unreadCount++
-      //   }
-      // }
+      for await (const notification of stream) {
+        notifStore.pushNotif(notification)
+        toast.add({
+          severity: 'info',
+          detail: notification.content,
+          summary: notification.title,
+        })
+        if (notification.unread) {
+          notifStore.unreadCount++
+        }
+      }
     } else {
       throw Error('User not defined')
     }
